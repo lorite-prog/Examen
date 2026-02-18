17b017d4664ccd05076c280e00c5ce90825d2aab
lorite-prog <loritemarcos@gmail.com>
2026-02-18 16:23:08 +0100

    Initial Commit

---
+++ Examen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Examen
+//
+//  Created by Tardes on 18/2/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
