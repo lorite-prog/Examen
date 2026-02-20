393666be8f9acea36457bcb72db6904c062f420d
lorite-prog <loritemarcos@gmail.com>
2026-02-20 16:23:41 +0100

    utils

--- Examen/DetailViewController.swift
+++ Examen/DetailViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class DetailViewController: UIViewController {
     
     var movie: Movie!
-    var movieName: String!
+    
     
     @IBOutlet weak var posterImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
